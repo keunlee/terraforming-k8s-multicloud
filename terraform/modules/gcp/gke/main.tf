450549e03ea1ad76ada20718039a5875e9bff787
keunlee <kelee@redhat.com>
2020-04-17 23:52:30 -0500

    add cloud nat

--- terraform/modules/gcp/gke/main.tf
+++ terraform/modules/gcp/gke/main.tf
@@ -80,6 +80,29 @@ resource "google_compute_subnetwork" "vpc_subnetwork" {
   ]
 }
 
+resource "google_compute_router" "router" {
+  name    = "${var.cluster_name}-router"
+  region  = google_compute_subnetwork.vpc_subnetwork.region
+  network = google_compute_network.vpc_network.self_link
+
+  bgp {
+    asn = 64514
+  }
+}
+
+resource "google_compute_router_nat" "nat" {
+  name                               = "${var.cluster_name}-router-nat"
+  router                             = google_compute_router.router.name
+  region                             = google_compute_router.router.region
+  nat_ip_allocate_option             = "AUTO_ONLY"
+  source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_IP_RANGES"
+
+  log_config {
+    enable = true
+    filter = "ERRORS_ONLY"
+  }
+}
+
 # https://www.terraform.io/docs/providers/google/r/container_cluster.html
 resource "google_container_cluster" "cluster" {
   provider = google-beta
